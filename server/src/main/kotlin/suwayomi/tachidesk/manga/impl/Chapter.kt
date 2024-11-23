a78df535558923069802c3edfe88321181ce9824
are-are-are <62763969+dejavui@users.noreply.github.com>
2024-11-24 06:39:03 +0700

    Update Chapter.kt

--- server/src/main/kotlin/suwayomi/tachidesk/manga/impl/Chapter.kt
+++ server/src/main/kotlin/suwayomi/tachidesk/manga/impl/Chapter.kt
@@ -147,6 +147,7 @@ object Chapter {
                     
                         description = manga.description
                     
+                        thumbnail_url = manga.thumbnail_url
                     }
 
                 val currentLatestChapterNumber = Manga.getLatestChapter(mangaId)?.chapterNumber ?: 0f
