c2c7868a3990c8f93a81444eea7e6e8be33699c6
are-are-are <62763969+dejavui@users.noreply.github.com>
2025-07-26 15:05:52 +0700

    update user agent

--- server/src/main/kotlin/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ server/src/main/kotlin/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -53,7 +53,7 @@ class NetworkHelper(
     private val userAgent =
         MutableStateFlow(
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
         )
     val userAgentFlow = userAgent.asStateFlow()
 
